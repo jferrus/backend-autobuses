04f964002981a32de3a436c9021cd5b833800435
Jose <jose.ferrus.94@gmail.com>
2025-01-19 13:59:52 +0100

    bbdd inixio

--- schema.sql
+++ schema.sql
@@ -11,6 +11,14 @@ CREATE TABLE IF NOT EXISTS precios_linea_1 (
   precio FLOAT
 );
 
+CREATE TABLE IF NOT EXISTS horarios_linea_1 (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  origen INTEGER,
+  direccion INTEGER,
+  salida TIME,
+  trayecto INTEGER
+);
+
 INSERT INTO paradas_linea_1 (nombre, opcional) VALUES ('Bicorp',true)
 INSERT INTO paradas_linea_1 (nombre, opcional) VALUES ('Quesa',true)
 INSERT INTO paradas_linea_1 (nombre, opcional) VALUES ('Navarrés',false)
@@ -66,4 +74,98 @@ INSERT INTO paradas_linea_1 (punto_a, punto_b, precio) VALUES (7,9, 1.9);
 INSERT INTO paradas_linea_1 (punto_a, punto_b, precio) VALUES (7,10, 2.25); 
 INSERT INTO paradas_linea_1 (punto_a, punto_b, precio) VALUES (8,9, 1.35); 
 INSERT INTO paradas_linea_1 (punto_a, punto_b, precio) VALUES (8,10, 1.55); 
-INSERT INTO paradas_linea_1 (punto_a, punto_b, precio) VALUES (9,10, 1.35)
+INSERT INTO paradas_linea_1 (punto_a, punto_b, precio) VALUES (9,10, 1.35);
+
+
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (1, 10, '5:50:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (1, 10, '7:30:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (1, 10, '13:00:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (1, 10, '16:07:00', 6);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (2, 10, '6:02:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (2, 10, '7:42:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (2, 10, '13:12:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (2, 10, '16:19:00', 6);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (3, 10, '6:12:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (3, 10, '7:55:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (3, 10, '10:15:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (3, 10, '13:22:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (3, 10, '16:29:00', 6);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (4, 10, '6:22:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (4, 10, '8:02:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (4, 10, '10:25:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (4, 10, '13:32:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (4, 10, '16:39:00', 6);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (5, 10, '6:27:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (5, 10, '8:09:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (5, 10, '10:32:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (5, 10, '13:39:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (5, 10, '16:46:00', 6);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (6, 10, '6:34:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (6, 10, '8:14:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (6, 10, '10:37:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (6, 10, '13:44:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (6, 10, '16:51:00', 6);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 10, '6:44:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 10, '7:37:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 10, '8:24:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 10, '10:47:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 10, '13:54:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 10, '17:01:00', 6);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (8, 10, '6:52:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (8, 10, '7:45:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (8, 10, '8:32:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (8, 10, '10:55:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (8, 10, '14:02:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (8, 10, '17:09:00', 6);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 10, '7:00:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 10, '8:00:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 10, '8:47:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 10, '11:10:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 10, '14:17:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 10, '17:24:00', 6);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (10, 10, '8:15:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (10, 10, '9:02:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (10, 10, '11:25:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (10, 10, '14:32:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (10, 1, '9:02:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (10, 1, '11:25:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (10, 1, '14:35:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 1, '7:07:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 1, '9:17:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 1, '11:40:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 1, '14:50:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (9, 1, '18:00:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (8, 1, '7:23:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (8, 1, '9:32:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (8, 1, '15:05:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (8, 1, '18:15:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 1, '7:37:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 1, '9:40:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 1, '12:03:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 1, '15:13:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (7, 1, '18:23:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (6, 1, '7:27:00', 1);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (6, 1, '9:50:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (6, 1, '12:13:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (6, 1, '15:23:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (6, 1, '18:33:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (5, 1, '9:55:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (5, 1, '12:18:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (5, 1, '15:28:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (5, 1, '18:38:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (4, 1, '10:02:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (4, 1, '12:25:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (4, 1, '15:35:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (4, 1, '18:45:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (3, 1, '10:12:00', 2);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (3, 1, '12:35:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (3, 1, '15:45:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (3, 1, '18:55:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (2, 1, '12:45:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (2, 1, '15:55:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (2, 1, '19:05:00', 5);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (1, 1, '12:57:00', 3);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (1, 1, '16:07:00', 4);
+INSERT INTO horarios_linea_1 (origen, direccion, salida, trayecto) VALUES (1, 1, '19:17:00', 5);
+
+
