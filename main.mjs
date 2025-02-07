183150554edd24c81e82935bbfdf375d54ac57b1
Jose <jose.ferrus.94@gmail.com>
2025-02-07 21:52:22 +0100

    cambios

--- main.mjs
+++ main.mjs
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // Replace with your allowed origin
+  res.header('Access-Control-Allow-Origin', process.env.SERVER); // Replace with your allowed origin
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
