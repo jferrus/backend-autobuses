042ce5489f0a0938dad6c4d05baedfa63e8bcbe3
Jose <jose.ferrus.94@gmail.com>
2025-02-08 19:20:03 +0100

    cambios

--- routes/horarios.mjs
+++ routes/horarios.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import {getHorariosDesdeAhaciaB} from '../utils/db.mjs'
 import log4js from 'log4js';
+import { param, validationResult } from 'express-validator';
 
 log4js.configure({
     appenders: { cheese: { type: "file", filename: "cheese.log" } },
@@ -14,16 +15,26 @@ var router = express.Router();
 /**
  * Muestra mediante el método GET una lista de horarios desde una parada A a una B.
  */
-router.get('/origen/:id_origen/destino/:id_destino', async function(req, res, next) {
+router.get('/origen/:id_origen/destino/:id_destino', 
+param('id_origen').trim().notEmpty().isLength({min:1, max:3}).isNumeric().isInt(),
+param('id_origen').trim().notEmpty().isLength({min:1, max:3}).isNumeric().isInt(),
+async function(req, res, next) {
 
-  const idOrigen = Number.parseInt(req.params.id_origen);
-  const idDestino = Number.parseInt(req.params.id_destino);
+  const result = validationResult(req);
+  if (result.isEmpty()) {
 
-  logger.info(`El id de origen es ${idOrigen} y el id de destino es ${idDestino}`);
+    const idOrigen = Number.parseInt(req.params.id_origen);
+    const idDestino = Number.parseInt(req.params.id_destino);
 
-  const nombres = await getHorariosDesdeAhaciaB(idOrigen, idDestino);
+    logger.info(`El id de origen es ${idOrigen} y el id de destino es ${idDestino}`);
 
-  res.status(200).json(nombres);
+    const nombres = await getHorariosDesdeAhaciaB(idOrigen, idDestino);
+
+    res.status(200).json(nombres);
+  } else {
+    logger.error(result.array());
+    res.status(500).json({'error': 'incorrect paramaters'});
+  }
 });
 
 export default router;
